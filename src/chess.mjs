d9a4f4a92f830b36a9f0da5465e16df10680663f
dukesook <devonsookhoo14@gmailcom>
2025-02-08 15:09:47 -0700

    create chess board

--- src/chess.mjs
+++ src/chess.mjs
@@ -1,1 +1,26 @@
 console.log('loading chess.mjs');
+
+const chessboard = document.getElementById('chessboard');
+
+
+const Chess = {
+
+  init: function() {
+    Chess.createBoard();
+  },
+
+  createBoard: function() {
+    for (let row = 0; row < 8; row++) {
+      for (let col = 0; col < 8; col++) {
+        const square = document.createElement('div');
+        square.innerHTML = `${row}, ${col}`;
+        chessboard.appendChild(square);
+      }
+    }
+  }
+
+}
+
+Chess.init();
+
+export default Chess;
