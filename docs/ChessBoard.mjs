021f9c259dd27f83e0d7da2ea4c72b1c2179e876
dukesook <devonsookhoo14@gmailcom>
2025-02-09 07:04:42 -0700

    add ChessBoard.must_be() checks

--- docs/ChessBoard.mjs
+++ docs/ChessBoard.mjs
@@ -103,9 +103,28 @@ export class BoardSquare {
     this.column = column;
   }
 
-  static must_be(object) {
+  static must_be(object, pieceStatus = null) {
     if (!(object instanceof BoardSquare)) {
       throw new Error('Must be a BoardSquare');
     }
+    if (!pieceStatus) {
+      return
+    }
+    const piece = object.piece;
+    
+    // Square must be occupied
+    if (pieceStatus == 'occupied') {
+      if (!piece) {
+        throw new Error('Square is empty');
+      }
+    }
+
+    // Square must be empty
+    if (pieceStatus == 'empty') {
+      if (piece) {
+        throw new Error('Square is not empty');
+      }
+    } 
+
   }
 }
