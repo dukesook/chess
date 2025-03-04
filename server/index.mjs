07309327bc1b4c98987e9a9091ab42b75ae00bae
dukesook <devonsookhoo14@gmailcom>
2025-03-03 21:24:42 -0700

    npm install express

--- server/index.mjs
+++ server/index.mjs
@@ -2,11 +2,12 @@
 // Node.js uses the V8 JavaScript engine from Google Chrome
 
 // $ npm init -y  // create package.json file
-// $ npm install express
-import express from 'express';
+import express from 'express'; // $ npm install express
+import socketio from 'socket.io'; // $ npm install socket.io
 import path from 'path';
 
-const app = express();
+const app = express(); // Express creates an HTTP server
+const io = socketio(app); // Socket.io creates a WebSocket server
 const PORT = 3000;
 
 // Serve static files from "public" folder (all .html, .css, & .js files)
