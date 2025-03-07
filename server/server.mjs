b17ea4b5d7a6c09e68a8c38c491e61c68f67f0fe
dukesook <devonsookhoo14@gmailcom>
2025-03-06 17:06:12 -0700

    rename index.mjs to server.mjs

--- server/server.mjs
+++ server/server.mjs
@@ -3,11 +3,10 @@
 
 // $ npm init -y  // create package.json file
 import express from 'express'; // $ npm install express
-import socketio from 'socket.io'; // $ npm install socket.io
+import { Server } from 'socket.io'; // $ npm install socket.io
 import path from 'path';
 
 const app = express(); // Express creates an HTTP server
-const io = socketio(app); // Socket.io creates a WebSocket server
 const PORT = 3000;
 
 // Serve static files from "public" folder (all .html, .css, & .js files)
